d5bbf79814fcc6d41bcbe0f070bd35ae77ab6cdf
Maimouna637 <81107301+Maimouna637@users.noreply.github.com>
2022-07-25 00:44:23 +0000

    Create accueilView.php

---
+++ Version2/Vue/accueilView.php
@@ -0,0 +1,22 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<style type="text/css">
+	body{
+		overflow-y: hidden;
+	}
+
+	body a {
+		text-decoration: none;
+		margin-left: 40%;
+
+	}
+</style>
+<body>
+		<b><a href="accueil.html" target="principale">Accueil</a></b>
+</body>
+</html>
