5c6df22b5fc4ffa10c4913110c6718a5b9b13a6d
Maimouna637 <81107301+Maimouna637@users.noreply.github.com>
2022-07-25 00:49:15 +0000

    Create siteView.php

---
+++ Version2/Vue/siteView.php
@@ -0,0 +1,41 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+
+<style type="text/css">
+	body{
+		background-image: url("rech.jpg");
+	}
+	.text{
+		display: flex;
+		position: relative;
+		font-weight: 400;
+		background-color: #FFF;
+	}
+</style>
+<body>
+		<div class="text">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas rhoncus dictum lectus ac dapibus. Quisque feugiat orci eget elit gravida suscipit. Curabitur eu lectus in nulla tempor fermentum. Sed laoreet magna non sem ornare eleifend. Aenean tristique, magna sit amet placerat maximus, tortor tellus egestas lacus, eu commodo tortor elit eu dolor. Suspendisse potenti. Integer quis fringilla eros, non consequat est. Integer feugiat pellentesque ipsum, non vehicula tellus mattis vel. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam rhoncus odio quis nulla feugiat auctor. Aenean dapibus in lacus vitae interdum. Suspendisse sit amet mollis enim.
+
+			</p>
+		</div>
+
+		<div class="text">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas rhoncus dictum lectus ac dapibus. Quisque feugiat orci eget elit gravida suscipit. Curabitur eu lectus in nulla tempor fermentum. Sed laoreet magna non sem ornare eleifend. Aenean tristique, magna sit amet placerat maximus, tortor tellus egestas lacus, eu commodo tortor elit eu dolor. Suspendisse potenti. Integer quis fringilla eros, non consequat est. Integer feugiat pellentesque ipsum, non vehicula tellus mattis vel. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam rhoncus odio quis nulla feugiat auctor. Aenean dapibus in lacus vitae interdum. Suspendisse sit amet mollis enim.
+
+			</p>
+		</div>
+
+		<div class="text">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas rhoncus dictum lectus ac dapibus. Quisque feugiat orci eget elit gravida suscipit. Curabitur eu lectus in nulla tempor fermentum. Sed laoreet magna non sem ornare eleifend. Aenean tristique, magna sit amet placerat maximus, tortor tellus egestas lacus, eu commodo tortor elit eu dolor. Suspendisse potenti. Integer quis fringilla eros, non consequat est. Integer feugiat pellentesque ipsum, non vehicula tellus mattis vel. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam rhoncus odio quis nulla feugiat auctor. Aenean dapibus in lacus vitae interdum. Suspendisse sit amet mollis enim.
+
+			</p>
+		</div>
+
+
+</body>
+</html>
