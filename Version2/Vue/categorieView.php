af5dce7d660871aec34ed6ea2888209760ae6248
Maimouna637 <81107301+Maimouna637@users.noreply.github.com>
2022-07-25 00:47:32 +0000

    Create categorieView.php

---
+++ Version2/Vue/categorieView.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+
+<style type="text/css">
+	body{
+		overflow-y: hidden;	
+	}
+
+	body a {
+		text-decoration: none;
+		margin-left: 40%;
+	}
+	
+</style>
+
+<body>
+	
+		<?php
+
+		$var = "<table border='1'>
+						<th>titre</th>
+						<tr><td>";
+
+		 
+
+			foreach ($chaine as $key => $value)
+			{
+				echo utf8_encode($chaine[$key]['libelle']);
+
+			}
+
+			$tab = $var.$valeur;
+				$tab = $tab."</td></tr></table>";
+
+				echo $tab;
+		?>
+			 
+		
+
+
+		
+</body>
+</html>
