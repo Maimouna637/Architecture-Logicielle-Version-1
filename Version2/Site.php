f1fe8f08d05b3bd6dba795a2a531ffaa50c58b14
Maimouna637 <81107301+Maimouna637@users.noreply.github.com>
2022-07-25 00:53:18 +0000

    Create Site.php

---
+++ Version2/Site.php
@@ -0,0 +1,40 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Actualités Polytechniciennes</title>
+</head>
+		<frameset rows="15%,5%,80%" >
+			<frame src="Vue/Actualités.php"></frame>
+
+			<frameset cols="25%,25%,25%,25%">
+				<frame src="Vue/accueilView.php"></frame>
+				<frame src="Vue/article.php"></frame>
+				<frame src="Vue/categorie.php"></frame>
+				<frame src="Vue/proposView.php"></frame>
+			</frameset>
+
+			<frame src="Vue/siteView.php" name="principale"></frame>
+			
+		</frameset>
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
