6704a8a0aab75bc122731e940d56bc79c51e58fc
Maimouna637 <81107301+Maimouna637@users.noreply.github.com>
2022-07-25 00:48:39 +0000

    Create proposView.php

---
+++ Version2/Vue/proposView.php
@@ -0,0 +1,21 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<style type="text/css">
+	body{
+		overflow-y: hidden;
+	}
+
+	body a {
+		margin-left:40%;
+		text-decoration: none;
+	}
+</style>
+<body>
+		<b><a href="propos.html" target="principale">A Propos</a></b>
+</body>
+</html>
