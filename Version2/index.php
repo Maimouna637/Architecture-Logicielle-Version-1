8361ec1f258bafeb1853204a152b9beec841b454
Maimouna637 <81107301+Maimouna637@users.noreply.github.com>
2022-07-25 00:51:39 +0000

    Create index.php

---
+++ Version2/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<style type="text/css">
+	body{
+		background-image: url('Vue/rech.jpg');
+		background-size: 100%
+	}
+
+	
+</style>
+<body>
+</body>
+</html>
+
+<?php 
+
+require_once 'Modele/connexion.php';
+require_once 'Controller/articleControl.php';
+require_once 'Modele/articleModele.php';
+	showArticle();
+ ?>
