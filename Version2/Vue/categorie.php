8b2785ff6029f4622d0d272ee7e9665d6127a195
Maimouna637 <81107301+Maimouna637@users.noreply.github.com>
2022-07-25 00:47:02 +0000

    Create categorie.php

---
+++ Version2/Vue/categorie.php
@@ -0,0 +1,21 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<style type="text/css">
+	body{
+		overflow-y: hidden;
+	}
+
+	body a {
+		margin-left: 40%;
+		text-decoration: none;
+	}
+</style>
+<body>
+		<b><a href="../indexCateg.php" target="principale">Categorie</a></b>
+</body>
+</html>
