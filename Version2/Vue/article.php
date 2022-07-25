478236f805cb1f0f2f1255dce56d7814004cdfc6
Maimouna637 <81107301+Maimouna637@users.noreply.github.com>
2022-07-25 00:45:29 +0000

    Create article.php

---
+++ Version2/Vue/article.php
@@ -0,0 +1,21 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<style type="text/css">
+	body{
+		overflow-y: hidden;
+	}
+
+	body a {
+		margin-left: 40%;
+		text-decoration: none;
+	}
+</style>
+<body>
+		<b><a href="../index.php" target="principale">Article</a></b>
+</body>
+</html>
