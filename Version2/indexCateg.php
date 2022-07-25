2c9d0727a9bb1480eb660f215cb7d0549d099692
Maimouna637 <81107301+Maimouna637@users.noreply.github.com>
2022-07-25 00:52:46 +0000

    Create indexCateg.php

---
+++ Version2/indexCateg.php
@@ -0,0 +1,26 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<style type="text/css">
+	body{
+		background-image: url('Vue/rech.jpg');
+		background-size: 100%
+	}
+
+	
+</style>
+<body>
+</body>
+</html>
+
+<?php 
+
+require_once 'Modele/connexion.php';
+require_once 'Controller/categorieController.php';
+require_once 'Modele/categorieModele.php';
+	showCategorie();
+ ?>
